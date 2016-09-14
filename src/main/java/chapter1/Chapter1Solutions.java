5d8cdc3fdd3500369fe05d464b40cad036b39ce9
Myyk Seok <myyk.seok@gmail.com>
2016-09-14 15:18:01 -0700

    Only up to 128 characters in ascii.

--- src/main/java/chapter1/Chapter1Solutions.java
+++ src/main/java/chapter1/Chapter1Solutions.java
@@ -9,16 +9,16 @@ public class Chapter1Solutions {
    * 
    * Assumptions: Characters are ASCII characters, capitalization matters
    * 
-   * Time complexity: O(min(c,n)) where c is number of characters in alphabet (256)
+   * Time complexity: O(min(c,n)) where c is number of characters in alphabet (128)
    * and n is the length of the string. Or O(1) technically.
-   * Space complexity: O(c) where c is number of characters in alphabet (256). Or O(1) technically.
+   * Space complexity: O(c) where c is number of characters in alphabet (128). Or O(1) technically.
    */
   public static boolean stringHasAllUniqueCharacters(String str) {
     // If there's more characters than there are in the alphabet, then there are duplicates.
-    if (str.length() > 256) {
+    if (str.length() > 128) {
       return false;
     } else {
-      boolean[] seen = new boolean[256];
+      boolean[] seen = new boolean[128];
       for (int i=0; i<str.length(); i++) {
         char c = str.charAt(i);
         if (seen[c]) {
@@ -34,18 +34,22 @@ public class Chapter1Solutions {
   /**
    * Check Permutation: Given 2 string check to see if one is a permutation of the other.
    * 
-   * Assumptions: Characters are ASCII characters, capitalization matters, strings aren't larger than 2^16.
+   * Assumptions:
+   *    ASCII characters
+   *    capitalization matters
+   *    whitespace matters
+   *    strings aren't larger than 2^16.
    * 
-   * Time complexity: O(max(c,n) where c is number of characters in alphabet (256)
+   * Time complexity: O(max(c,n) where c is number of characters in alphabet (128)
    * and n is the length of the string.
-   * Space complexity: O(c) where c is number of characters in alphabet (256). Or O(1) technically.
+   * Space complexity: O(c) where c is number of characters in alphabet (128). Or O(1) technically.
    */
   public static boolean arePermutations(String a, String b) {
     // Can't be permutations if the same length
     if (a.length() != b.length()) {
       return false;
     } else {
-      short[] occurances = new short[256];
+      short[] occurances = new short[128];
       for (int i=0; i<a.length(); i++) {
         occurances[a.charAt(i)]++;
         occurances[b.charAt(i)]--;        
