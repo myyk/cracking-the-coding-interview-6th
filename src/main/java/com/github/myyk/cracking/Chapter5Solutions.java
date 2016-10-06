beef1228b335784f1a32cba550b7ed5f19360877
Myyk Seok <myyk.seok@gmail.com>
2016-10-06 14:44:21 -0700

    Add insert int into int method.

--- src/main/java/com/github/myyk/cracking/Chapter5Solutions.java
+++ src/main/java/com/github/myyk/cracking/Chapter5Solutions.java
@@ -6,12 +6,19 @@ package com.github.myyk.cracking;
 public class Chapter5Solutions {
 
   /**
-   * Route Between Nodes: Find whether there is a path between two nodes (A->B) in a directed graph.
+   * Insertion: Given 2 32-bit numbers N and M and two indexes i and j. Insert M into M such that it starts
+   * at bit j and ends at bit i.
    *
    * Assumptions:
+   *   i and j have enough bits between them to fit M.
+   *   if there are already bits set in N between i and j, they will be overwritten
    *
    * Time complexity: O(n)
    * Space complexity: O(n)
    */
-  
+  public static int insert(int n, int m, int j, int i) {
+    n &= (-1 << (j + 1)) | ((1 << i) - 1);
+    n |= (m << i);
+    return n;
+  }
 }
--- src/test/scala/com/github/myyk/cracking/Chapter5SolutionsTest.scala
+++ src/test/scala/com/github/myyk/cracking/Chapter5SolutionsTest.scala
@@ -4,5 +4,40 @@ import org.scalatest.FlatSpec
 import org.scalatest.Matchers
 
 class Chapter5SolutionsTest extends FlatSpec with Matchers {
-  
+  implicit def enrichStringContext(sc: StringContext) = new RichStringContext(sc)
+
+  class RichStringContext(sc: StringContext) {
+    def b() = {
+      def parseBinary(s: String): Int = {
+        var i = s.length - 1
+        var sum = 0
+        var mult = 1
+        while (i >= 0) {
+          s.charAt(i) match {
+            case '1' => sum += mult
+            case '0' =>
+          }
+          mult *= 2
+          i -= 1
+        }
+        sum
+      }
+      val s = sc.parts.mkString
+      parseBinary(s)
+    }
+
+  }
+
+  "insert" should "insert the integer into the other" in {
+    //    Chapter5Solutions.insert(0, 0, 0, 0) shouldBe 0
+    Chapter5Solutions.insert(1, 1, 0, 0) shouldBe 1
+    Chapter5Solutions.insert(1, 1, 0, 0) shouldBe 1
+    Chapter5Solutions.insert(2, 1, 0, 0) shouldBe 3
+    Chapter5Solutions.insert(1, 1, 1, 1) shouldBe 3
+    Chapter5Solutions.insert(1, b"111", 8, 6) shouldBe b"111000001"
+    Chapter5Solutions.insert(b"101000000000", b"1101", 4, 2) shouldBe b"101000110100"
+    Chapter5Solutions.insert(~0, b"1101", 4, 2) shouldBe ~0 ^ (b"1111"<<2) | (b"1101"<<2)
+    Chapter5Solutions.insert(b"111111", b"1010", 4, 1) shouldBe b"110101"
+  }
+
 }
