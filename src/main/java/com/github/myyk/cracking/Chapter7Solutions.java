1c77abd547d71ae4fd6eb4d8c4db08a688207d40
Myyk Seok <myyk.seok@gmail.com>
2016-10-13 18:36:34 -0700

    add tripleStepIterative

--- src/main/java/com/github/myyk/cracking/Chapter7Solutions.java
+++ src/main/java/com/github/myyk/cracking/Chapter7Solutions.java
@@ -1,5 +1,6 @@
 package com.github.myyk.cracking;
 
+import java.math.BigInteger;
 import java.util.List;
 
 import com.google.common.collect.Lists;
@@ -14,21 +15,53 @@ public class Chapter7Solutions {
    *
    * Assumptions:
    *
-   * Time complexity: O()
-   * Space complexity: O()
+   * Time complexity: O(n)
+   * Space complexity: O(n)
    */
-  public static int tripleStep(int steps) {
-    return tripleStep(steps, Lists.newArrayList(1, 1, 2));
+  public static BigInteger tripleStepRecursive(final int steps) {
+    return tripleStepRecursive(steps, Lists.newArrayList(BigInteger.valueOf(1), BigInteger.valueOf(1), BigInteger.valueOf(2)));
   }
 
-  private static int tripleStep(int steps, List<Integer> stepsToWays) {
+  private static BigInteger tripleStepRecursive(final int steps, final List<BigInteger> stepsToWays) {
+    System.out.println("steps = " + steps + "    stepsToWays = " + stepsToWays.toString());
     if (steps < 0) {
-      return 0;
+      return BigInteger.ZERO;
     } else if (steps < stepsToWays.size()) {
       return stepsToWays.get(steps);
     } else {
-      stepsToWays.add(tripleStep(steps - 1) + tripleStep(steps - 2) + tripleStep(steps - 3));
-      return stepsToWays.get(steps);
+      BigInteger ways = tripleStepRecursive(steps - 1, stepsToWays).add(tripleStepRecursive(steps - 2, stepsToWays).add(tripleStepRecursive(steps - 3, stepsToWays)));
+      stepsToWays.add(ways);
+      return ways;
+    }
+  }
+
+  /**
+   * Triple Step: Same as above but using less space and coded iteratively.
+   *
+   * Assumptions:
+   *
+   * Time complexity: O(n)
+   * Space complexity: O(1)
+   */
+  public static BigInteger tripleStepIterative(int steps) {
+    if (steps < 0) {
+      return BigInteger.ZERO;
+    } else if (steps == 0 || steps == 1) {
+      return BigInteger.ONE;
+    } else if (steps == 2) {
+      return BigInteger.valueOf(2);
+    } else {
+      BigInteger nMinus3 = BigInteger.ONE;
+      BigInteger nMinus2 = BigInteger.ONE;
+      BigInteger nMinus1 = BigInteger.valueOf(2);
+
+      for (int i = 3; i < steps; i++) {
+        BigInteger temp = nMinus1.add(nMinus2.add(nMinus3));
+        nMinus3 = nMinus2;
+        nMinus2 = nMinus1;
+        nMinus1 = temp;
+      }
+      return nMinus1.add(nMinus2.add(nMinus3));
     }
   }
 }
--- src/test/scala/com/github/myyk/cracking/Chapter7SolutionsTest.scala
+++ src/test/scala/com/github/myyk/cracking/Chapter7SolutionsTest.scala
@@ -4,11 +4,19 @@ import org.scalatest.FlatSpec
 import org.scalatest.Matchers
 
 class Chapter7SolutionsTest extends FlatSpec with Matchers {
-  "tripleStep" should "count the ways the child can go up the stairs with 1, 2, or 3 steps at a time" in {
-    Chapter7Solutions.tripleStep(0) shouldBe 1
-    Chapter7Solutions.tripleStep(1) shouldBe 1
-    Chapter7Solutions.tripleStep(2) shouldBe 2
-    Chapter7Solutions.tripleStep(3) shouldBe 4
+  def testTripleStep(tripleStep: Int => BigInt): Unit = {
+    tripleStep(0) shouldBe 1
+    tripleStep(1) shouldBe 1
+    tripleStep(2) shouldBe 2
+    tripleStep(3) shouldBe 4
+    tripleStep(4) shouldBe 7
+    tripleStep(5) shouldBe 13
   }
-  
+
+  "tripleStepRecursive" should "count the ways the child can go up the stairs with 1, 2, or 3 steps at a time" in {
+    testTripleStep(Chapter7Solutions.tripleStepRecursive)
+  }
+  "tripleStepIterative" should "count the ways the child can go up the stairs with 1, 2, or 3 steps at a time" in {
+    testTripleStep(Chapter7Solutions.tripleStepIterative)
+  }
 }
